6e4ddf888f2a7a41dbab9814dd58404f02827704
ltfschoen <ltfschoen@gmail.com>
2014-07-21 14:50:43 +1000

    Added View and Indicator to Storyboard

--- BlueLibrary/ViewController.h
+++ BlueLibrary/ViewController.h
@@ -5,6 +5,7 @@
 //  Created by Eli Ganem on 31/7/13.
 //  Copyright (c) 2013 Eli Ganem. All rights reserved.
 //
+//  Modified by Luke Schoen in 2014
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
--- BlueLibrary/ViewController.m
+++ BlueLibrary/ViewController.m
@@ -5,19 +5,35 @@
 //  Created by Eli Ganem on 31/7/13.
 //  Copyright (c) 2013 Eli Ganem. All rights reserved.
 //
+//  Modified by Luke Schoen in 2014
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
+// private properties not required by other classes
+@property (weak, nonatomic) IBOutlet UIImageView *coverImage;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
+
 @end
 
 @implementation ViewController
+
+@synthesize coverImage;
+@synthesize indicator;
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    self.coverImage.backgroundColor = [UIColor redColor];
+    self.indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
+    [self.indicator startAnimating];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
