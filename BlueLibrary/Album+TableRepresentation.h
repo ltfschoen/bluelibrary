7e036c1a36870ce51f79278c3d71bfa564f5c3cd
ltfschoen <ltfschoen@gmail.com>
2014-07-22 08:59:43 +1000

    Added Category (Decorator Design Pattern) to Extend the Album Class
    Creates a Data Structure representation of the Data Model with NSDictionary

---
+++ BlueLibrary/Album+TableRepresentation.h
@@ -0,0 +1,13 @@
+//
+//  Album+TableRepresentation.h
+//  BlueLibrary
+//
+//  Created by Luke on 22/07/2014.
+//  Copyright (c) 2014 Eli Ganem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Album_TableRepresentation : NSObject
+
+@end
---
+++ BlueLibrary/Album+TableRepresentation.m
@@ -0,0 +1,13 @@
+//
+//  Album+TableRepresentation.m
+//  BlueLibrary
+//
+//  Created by Luke on 22/07/2014.
+//  Copyright (c) 2014 Eli Ganem. All rights reserved.
+//
+
+#import "Album+TableRepresentation.h"
+
+@implementation Album_TableRepresentation
+
+@end
