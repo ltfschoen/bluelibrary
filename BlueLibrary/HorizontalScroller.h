41da0299f1e2d76d4d62803d967d6a62dbb5d2d0
ltfschoen <ltfschoen@gmail.com>
2014-07-22 10:55:14 +1000

    Added Forward Declaration for HorizontalScroller Protocol

--- BlueLibrary/HorizontalScroller.h
+++ BlueLibrary/HorizontalScroller.h
@@ -8,11 +8,60 @@
 
 #import <UIKit/UIKit.h>
 
+//
+//  refer to new delegate in class definition. protocol is now visible at this point
+//  use forward declaration of the protocol so Xcode and compiler know it will be available
+//
+
+@protocol HorizontalScrollerDelegate;
+
 @interface HorizontalScroller : UIView
+
+    //
+    // property with weak pointer to its delegate to prevent a strong reference retain cycle
+    // (where the delegate keeps a strong pointer back to the conforming class that has a strong pointer to the delegate as well)
+    // resulting in memory leak since neither class will release the memory allocated to each other
+    //
+    // use 'id' type for type safety (delegate can only be assigned classes conforming to HorizontalScrollerDelegate
+    //
+
+    @property (weak) id<HorizontalScrollerDelegate> delegate;
+
+    //
+    // reload method modelled after reloadData in UITableView. it reloads all the data used to construct the horizontal scroller.
+    //
+
+    - (void)reload;
 
 @end
 
+//
+//  protocol conforms to inherited NSObject protocol is best practice
+//  and enables sending of messages defined by NSObject to the
+//  HorizontalScroller Delegate
+//
+
 @protocol HorizontalScrollerDelegate <NSObject>
-    // methods declaration goes in here
+
+    //
+    //  define the required and optional methods for the delegate to implement
+    //
+
+    @required
+
+    // number of views (ask delegate how many views want to present inside horizontal scroller)
+    - (NSInteger)numberOfViewsForHorizontalScroller:(HorizontalScroller*)scroller;
+
+    // view at specific index (ask delegate to return the view that should appear at <index>)
+    - (UIView*)horizontalScroller:(HorizontalScroller*)scroller viewAtIndex:(int)index;
+
+    // behaviour when tapped (inform delegate which view at <index> has been clicked)
+    - (void)horizontalScroller:(HorizontalScroller*)scroller clickedViewAtIndex:(int)index;
+
+    @optional
+
+    // initial view (ask delegate for index of initial view to display. this method is optional
+    // and defaults to 0 if it is not implemented by the delegate)
+    - (NSInteger)initialViewIndexForHorizontalScroller:(HorizontalScroller*)scroller;
 
 @end
--- BlueLibrary/HorizontalScroller.m
+++ BlueLibrary/HorizontalScroller.m
@@ -8,6 +8,44 @@
 
 #import "HorizontalScroller.h"
 
+//
+//  constants defined for view within the enclosing scroller to simplfiy modification of the layout when designing
+//
+
+#define VIEW_PADDING 10
+#define VIEW_DIMENSIONS 100
+#define VIEWS_OFFSET 100
+
+
+//
+//  HorizontalScroller conforms to the UIScrollViewDelegate protocol
+//  HorizontalScroller uses UIScrollView to scroll the album covers
+//  HorizontalScroller needs to know user interaction events (i.e. user stops scrolling)
+//
+
+@interface HorizontalScroller () <UIScrollViewDelegate>
+
+@end
+
+//  create a scrolling view that contains the album cover views
+
 @implementation HorizontalScroller
+{
+    UIScrollView *scroller;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        scroller = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+        scroller.delegate = self;
+        [self addSubview:scroller];
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollerTapped:)];
+        [scroller addGestureRecognizer:tapRecognizer];
+    }
+    return self;
+}
 
 @end
