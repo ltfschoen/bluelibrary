92adb558766b221b0cfbd2655aadea370781a5fe
ltfschoen <ltfschoen@gmail.com>
2014-07-22 22:37:30 +1000

    Added KVO observer to stop the indicator rotating upon completion of album image

--- BlueLibrary/AlbumView.m
+++ BlueLibrary/AlbumView.m
@@ -38,6 +38,12 @@
         [self addSubview:indicator];
         
         //
+        //  KVO pattern - add the current class as an observer for the 'image' property of album coverImage
+        //
+        
+        [coverImage addObserver:self forKeyPath:@"image" options:0 context:nil];
+        
+        //
         //  perform album cover image download task
         //  notification sent through NSNotificationCenter singleton
         //  contains UIImageView to populate URL and cover image to be downloaded
@@ -50,5 +56,27 @@
     }
     return self;
 }
+
+//
+//  observer implemented in all classes acting as an observer.
+//  the system executes this method each time the observed property changes
+//
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"image"])
+    {
+        [indicator stopAnimating];
+    }
+}
+
+//
+//  unregister the KVO observer when it has downloaded
+//
+
+//- (void)dealloc
+//{
+//    [coverImage removeObserver:self forKeyPath:@"image"];
+//}
 
 @end
